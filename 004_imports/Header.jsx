3791af107dd21f898f0473bcb4adc9be09b9fd96
NIKESH <nikeshnoida@gmail.com>
2026-03-21 22:51:17 +0530

    Add files via upload

---
+++ 004_imports/Header.jsx
@@ -0,0 +1,16 @@
+function HEAD(){
+    return(
+        <h1>WORLD</h1>
+    )
+}
+export function Card(){
+    return(
+        <h1>this is a card</h1>
+    )
+}
+export function Card2(){
+    return(
+        <h1>this is a card2</h1>
+    )
+}
+export default HEAD
